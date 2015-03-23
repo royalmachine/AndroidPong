e822bb5c73b4370ddbcadfc0580cd3dc7834ce9d
fir3walk3r <fir3walk3r@abv.bg>
2015-03-23 11:48:41 +0100

    potato679

--- app/src/main/java/com/example/teodor/androidpong/MainActivity.java
+++ app/src/main/java/com/example/teodor/androidpong/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-//778788
+//7787889
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
